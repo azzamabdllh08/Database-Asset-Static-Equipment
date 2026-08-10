16d9fcfa6714e20466f9c8b5cf90eb298d7abb51
azzamabdllh08 <azzamabdllh08@gmail.com>
2026-08-10 16:20:08 +0700

    Add initial static equipment database

---
+++ data.js
@@ -0,0 +1,55 @@
+const ASSETS = [
+  {
+    tag: "DEMO-V-001",
+    name: "Feed Separator",
+    type: "Vessel",
+    area: "Area 01",
+    service: "Process Gas",
+    material: "SA-516 Gr.70",
+    designPressure: "10 barg",
+    designTemperature: "120 °C",
+    nominalThickness: 12,
+    minThickness: 9.5,
+    currentThickness: 11.2,
+    corrosionRate: 0.10,
+    damageMechanism: "Internal Thinning",
+    risk: "3C",
+    rbiStatus: "Assessment"
+  },
+  {
+    tag: "DEMO-T-001",
+    name: "Storage Tank",
+    type: "Tank",
+    area: "Area 02",
+    service: "Crude",
+    material: "SA-516 Gr.70",
+    designPressure: "0.5 barg",
+    designTemperature: "60 °C",
+    nominalThickness: 10,
+    minThickness: 7.5,
+    currentThickness: 8.1,
+    corrosionRate: 0.08,
+    damageMechanism: "Atmospheric Corrosion",
+    risk: "2C",
+    rbiStatus: "Assessment"
+  },
+  {
+    tag: "DEMO-E-001",
+    name: "Heat Exchanger",
+    type: "Heat Exchanger",
+    area: "Area 03",
+    service: "Cooling Water",
+    material: "Carbon Steel",
+    designPressure: "8 barg",
+    designTemperature: "90 °C",
+    nominalThickness: 8,
+    minThickness: 5.8,
+    currentThickness: 7.1,
+    corrosionRate: 0.05,
+    damageMechanism: "Erosion-Corrosion",
+    risk: "1C",
+    rbiStatus: "Assessment"
+  }
+];
+
+const INSPECTIONS = [];
