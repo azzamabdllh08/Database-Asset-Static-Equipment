94f462519b5c0593f93853c3a5439ab84af82501
azzamabdllh08 <azzamabdllh08@gmail.com>
2026-08-10 16:25:34 +0700

    Configure Report folder

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
   brand: "DATABASE ASSET STATIC EQUIPMENT",
   title: "DATABASE ASSET STATIC EQUIPMENT",
-  reportUrl: ""
+  reportUrl: "https://1drv.ms/f/c/8dd2c5d0a467a39a/IgBGq-_P3IkZTYk2rsWBkd-VAb1oNTqVs2J6KM667FCvxqc?e=GYRHCX",
+  databaseTemplate: "asset_template.csv"
 };
