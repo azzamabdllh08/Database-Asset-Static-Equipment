a376d019089146c2b66b20f6b11ea802b204f3be
azzamabdllh08 <azzamabdllh08@gmail.com>
2026-08-12 15:38:35 +0700

    Load dashboard UI and theme layer

--- config.js
+++ config.js
@@ -4,3 +4,8 @@ const CONFIG = {
   reportUrl: "https://1drv.ms/f/c/8dd2c5d0a467a39a/IgBGq-_P3IkZTYk2rsWBkd-VAb1oNTqVs2J6KM667FCvxqc?e=GYRHCX",
   databaseTemplate: "asset_template.csv"
 };
+
+(()=>{
+  const css=document.createElement('link');css.rel='stylesheet';css.href='dashboard_theme.css';document.head.appendChild(css);
+  const js=document.createElement('script');js.src='dashboard_ui.js';document.head.appendChild(js);
+})();
